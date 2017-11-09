83a3c6837f35b7cc3e776fd727335a414913c66e
akyekth <akye@kth.se>
2017-11-09 12:18:51 +0100

    Adding date to sensemaking

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -40,3 +40,26 @@ $(function() {
         }
     }
 });
+ $(".dropdown-menu li a").click(function(){
+            var selText = $(this).text();
+            $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+      });
+ $( function() {
+             $( "#datetimepicker1" ).datetimepicker({
+                buttonImage: "/images/datepicker.gif"
+            });
+    });   
+    
+
+  $(function () {
+                $('#datetimepicker6').datetimepicker();
+                $('#datetimepicker7').datetimepicker({
+                    useCurrent: false //Important! See issue #1075
+                });
+                $("#datetimepicker6").on("dp.change", function (e) {
+                    $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+                });
+                $("#datetimepicker7").on("dp.change", function (e) {
+                    $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
+    });    
